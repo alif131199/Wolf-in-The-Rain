a5ef17de400efcfe0aca9b2386f335ec0661e3f1
alif131199 <39584897+alif131199@users.noreply.github.com>
2019-11-29 09:11:12 +0700

    initial commit

---
+++ Data Project/Wolf/Assets/Script/TargetControl.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TargetControl : MonoBehaviour
+{
+    Animator anim;
+	int gauge = 0;
+	int back = 0;
+	public GameObject player;
+	public GameObject alert;
+
+    void Start()
+    {
+	anim = GetComponent<Animator>();
+	alert.GetComponent<Renderer>().material.color = Color.green;
+    }
+
+    void Update()
+    {
+
+	gauge+=Random.Range(0,10);
+
+
+     if (gauge>=900){
+			anim.SetInteger("aware",1);
+			//transform.Rotate(0,180,0);
+		}
+	  if(gauge>=1000 && back == 0){
+	  	  back = 1;
+	  }
+
+	  if (gauge>=2000){
+			anim.SetInteger("aware",0);
+			//transform.Rotate(0,180,0);
+			gauge = 0;
+			back = 0;
+		}
+
+
+		if(player.GetComponent<ThirdPersonCharacterControl>().move == 1 && back == 1){
+			//alert.GetComponent<Renderer>().material.color = Color.red;
+			SceneManager.LoadScene ("GameOver");
+		}
+
+		if(back == 0){
+			//alert.GetComponent<Renderer>().material.color = Color.green;
+		}
+		
+    }
+
+	void OnCollisionEnter(Collision collisionInfo){
+		if(collisionInfo.collider.tag == "Player"){
+			//alert.GetComponent<Renderer>().material.color = Color.red;
+			SceneManager.LoadScene ("Win");
+		}
+	}
+}
---
+++ Data Project/Wolf/Assets/Script/ThirdPersonCharacterControl.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCharacterControl : MonoBehaviour
+{
+    public float Speed;
+
+	public int move = 0;
+
+	Animator anim;
+
+	void Start(){
+		anim = GetComponent<Animator>();
+	}
+
+	void Update ()
+    {
+        PlayerMovement();
+		if (Input.GetKey (KeyCode.W)){
+			anim.SetInteger ("condition",1);
+		}
+
+		if (Input.GetKeyUp (KeyCode.W)){
+			anim.SetInteger ("condition",0);
+		}
+
+		if (Input.GetKey (KeyCode.S)){
+			anim.SetInteger ("condition",2);
+		}
+
+		if (Input.GetKeyUp (KeyCode.S)){
+			anim.SetInteger ("condition",0);
+		}
+
+		if (Input.GetKey (KeyCode.A)){
+			anim.SetInteger ("condition",3);
+		}
+
+		if (Input.GetKeyUp (KeyCode.A)){
+			anim.SetInteger ("condition",0);
+		}
+
+		if (Input.GetKey (KeyCode.D)){
+			anim.SetInteger ("condition",4);
+		}
+
+		if (Input.GetKeyUp (KeyCode.D)){
+			anim.SetInteger ("condition",0);
+		}
+
+		if(anim.GetInteger("condition")>0){
+			move = 1;
+		}
+		else{
+			move = 0;
+		}
+    }
+
+    void PlayerMovement()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+        Vector3 playerMovement = new Vector3(hor, 0f, ver) * Speed * Time.deltaTime;
+        transform.Translate(playerMovement, Space.Self);
+    }
+}
